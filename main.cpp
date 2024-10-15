e9a5d0b100cc60fef0f7026a76094d86c0e0b774
Hangyul Yi <hangyulyi3@gmail.com>
2024-10-15 15:38:25 -0600

    more attributes (adding multiple color on one shape)

--- main.cpp
+++ main.cpp
@@ -10,17 +10,21 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char *vertexShaderSource = "#version 330 core \n"
+const char *vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
+	"layout (location = 1) in vec3 aColor;\n"
+	"out vec3 ourColor;\n"
 	"void main()\n"
 	"{\n"
-	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"	gl_Position = vec4(aPos, 1.0);\n"
+	"	ourColor = aColor;\n"
 	"}\0";
 const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
+	"in vec3 ourColor;\n"
 	"void main()\n"
 	"{\n"
-	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"	FragColor = vec4(ourColor, 1.0f);\n"
 	"}\n\0";
 
 
@@ -32,7 +36,6 @@ int main()
 	// openGL version 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 # ifdef __APPLE__
@@ -110,9 +113,10 @@ int main()
 	// set up vertex data (and buffer(s)) and config vertex attributes
 	// ---------------------------------------------------------------
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f, // left
-		 0.5f, -0.5f, 0.0f, // right
-		 0.5f,  0.5f, 0.0f // top
+		// positions		// colors
+		 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // right
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // left
+		 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f  // top
 	};
 
 	unsigned int VBO, VAO;
@@ -124,13 +128,16 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// modifying other VAOs requires a call to glBindVertexArray so generally don't unbind VAOs (nor VBOs) when not directly necessary
-	glBindVertexArray(0);
+	glBindVertexArray(VAO);
 	
 
 	// render loop
@@ -149,8 +156,16 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// draw triange
+		// activate shader before any calls
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
+
+		// update shader uniform
+		double timeValue = glfwGetTime();
+		float greenValue = static_cast<float>(sin(timeValue) / 2.0 + 5.0);
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+		// render the triangle
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
