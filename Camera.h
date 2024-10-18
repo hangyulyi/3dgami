d5c2e0ce12e54c6f4655e33ceb0a5b9c2d8b86dc
Hangyul Yi <hangyulyi3@gmail.com>
2024-10-17 22:17:38 -0600

    create camera header

---
+++ Camera.h
@@ -0,0 +1,40 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+// used to define options for camera movement
+enum CAMERA_MOVEMENT
+{
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+const float YAW			= -90.0f;
+const float PITCH		= 0.0f;
+const float SPEED		= 2.5f;
+const float SENSITIVITY = 0.1f;
+const float ZOOM		= 45.0f;
+
+class Camera
+{
+public:
+	glm::vec3 Position;
+	glm::vec3 Front;
+	glm::vec3 Up;
+	glm::vec3 Right;
+	glm::vec3 WorldUp;
+	// euler angles
+	float Yaw;
+	float Pitch;
+	// camera options
+	float MovementSpeed;
+	float MouseSensitivity;
+	float Zoom;
+};
+
+#endif // !CAMERA_H
